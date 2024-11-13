e38f66f1f39b11c07b7f4907f47ffb52beca5404
defe014 <federicoaosandon@gmail.com>
2024-11-13 13:55:54 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { HomePage } from "@/components/HomePage";
-import Image from "next/image";
+
 
 export default function Home() {
     return (
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { use, useRef } from "react"
+import { useRef } from "react"
 import gsap from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
 import { useGSAP } from "@gsap/react"
